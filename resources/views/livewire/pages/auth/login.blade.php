212e550b7c7dd49ac782ca219ab51c642ebcd701
newhart <dev.haritianakely@gmail.com>
2024-01-22 18:08:00 +0300

    fix(login) : change  redirect

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -21,10 +21,7 @@ new #[Layout('layouts.guest')] class extends Component
 
         Session::regenerate();
 
-        $this->redirect(
-            session('url.intended', RouteServiceProvider::HOME),
-            navigate: true
-        );
+        redirect()->to( RouteServiceProvider::HOME);
     }
 }; ?>
 
